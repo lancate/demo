a7d8af539fe2c7ae5a1a4c05a5829d21c9fae08e
Harun <lassassinyc@gmail.com>
2020-10-02 20:48:00 +0200

    Rjesenje 2. zadatka

--- drugi.cpp
+++ drugi.cpp
@@ -1,8 +1,29 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
+struct trougao {
+	int stranica1,stranica2,stranica3;
+};
+
+double povrsina(int prva,int druga,int treca){
+	double prvii=static_cast<double>(prva);
+	double s{(prvii+druga+treca)/2};
+	double rezultat=sqrt(s*(s-prvii)*(s-druga)*(s-treca));
+	return rezultat;
+}
 
 
 int main (){
-	
+	cout<<"Unesi stranice prvog trougla: ";
+	int a,b,c;
+	cin>>a>>b>>c;
+	trougao prvi{a,b,c};
+	cout<<"Unesi stranice drugog trougla: ";
+	cin>>a>>b>>c;
+	trougao drugi{a,b,c};
+	cout<<"Povrsina prvog trougla: "<<povrsina(prvi.stranica1,prvi.stranica2,prvi.stranica3)<<endl;
+	cout<<"Povrsina drugog trougla: "<<povrsina(drugi.stranica1,drugi.stranica2,drugi.stranica3)<<endl;
+	cout<<"Razlika izmedju drugog i prvog trougla je "<<povrsina(drugi.stranica1,drugi.stranica2,drugi.stranica3)/
+	povrsina(prvi.stranica1,prvi.stranica2,prvi.stranica3)<<endl;
 }
