c6f111de72423c3ce46e435e9d883b9fc889a9db
Harun <lassassinyc@gmail.com>
2020-10-03 22:37:13 +0200

    Gotovo

--- treci.cpp
+++ treci.cpp
@@ -1,4 +1,21 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
+
+struct tacka{
+	int x,y;
+};
+
+double udaljenost(int a,int b){
+	return static_cast<double>(sqrt(pow(0-a,2)+pow(0-b,2)));
+}
+
+
 int main (){
+	cout<<"Unesi koordinate tacke(x,y): ";
+	int a,b;
+	cin>>a>>b;
+	tacka jedan{a,b};
+	cout<<"Udaljenost prve tacke od pocetka sistema(0,0) je "<<udaljenost(jedan.x,jedan.y);
+	
 }
